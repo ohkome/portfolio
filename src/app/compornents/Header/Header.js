a00bbfd87d2d48a660f95b7fc5f6d4094655e331
ohkome <workskome@gmail.com>
2023-08-30 21:23:20 +0900

    お試し

--- src/app/compornents/Header/Header.js
+++ src/app/compornents/Header/Header.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './header.module.css'
+import Link from 'next/link'
 //コンポーネント
-import Logo from './Logo'
+import Logo from '../Logo/Logo'
 
 const Header = () => {
   return (
-    <div className={styles.Header}>Header
+    <div className={styles.Header}>
+      <Link href="/">
         <Logo />
+      </Link>
+      
     </div>
   );
 }
