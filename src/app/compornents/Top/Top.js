0ded18d8ef444754434e6aa0dad2ebadd01278f8
ohkome <workskome@gmail.com>
2023-08-30 22:49:32 +0900

    topのレイアウト

--- src/app/compornents/Top/Top.js
+++ src/app/compornents/Top/Top.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import Image from 'next/image'
 import styles from './Top.module.css'
+
 
 // コンポーネント
 import Header from '../Header/Header'
@@ -8,7 +10,25 @@ import Header from '../Header/Header'
 const Top = () => {
   return (
     <div className={styles.grid}>
-      <Header />
+      <Header className={styles.header}/>
+      <div className={styles.img}>
+        <Image 
+        src='/photo.png' 
+        alt="me" 
+        priority
+        width={300}
+        height={400}
+        />
+        <p className={styles.comment}>Welcom!</p>
+      </div>
+      <div className={styles.title}>
+        <h1>
+          hello
+        </h1>
+        <p>
+            This is My Portfolio.
+        </p>
+      </div>
     </div>
   )
 }
