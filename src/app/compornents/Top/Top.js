3c497489dbaf927f69b099b80c3d4146a5432a03
ohkome <workskome@gmail.com>
2023-09-06 20:32:30 +0900

    ヘッダーコンポーネントを一旦削除し、トップに組み込む

--- src/app/compornents/Top/Top.js
+++ src/app/compornents/Top/Top.js
@@ -1,16 +1,52 @@
+
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './Top.module.css'
 
 
 // コンポーネント
-import Header from '../Header/Header'
+import Logo from '../Logo/Logo'
 
+
+const navigationMenu = [
+  {
+    href: "#",
+    label: "Home",
+  },
+  {
+    href: "#",
+    label: "About",
+  },
+  {
+    href: "#",
+    label: "Works",
+  },
+]
 
 const Top = () => {
   return (
     <div className={styles.grid}>
-      <Header className={styles.header}/>
+      <header className={styles.header}>
+        {/* svg-logo */}
+        <Link href="/" className={styles.link} >
+          <Logo />
+        </Link>
+
+        {/* navigationMenu */}
+        <nav className={styles.nav}>
+          <ul className={styles.flex}>
+            {navigationMenu.map((item)=>{
+              return(
+                <li key={item.label} className={styles.item}>
+                <Link href={item.href} className={styles.link}>{item.label}</Link>
+              </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+
       <div className={styles.img}>
         <Image 
         src='/photo.png' 
@@ -19,8 +55,8 @@ const Top = () => {
         width={300}
         height={400}
         />
-        <p className={styles.comment}>Welcom!</p>
       </div>
+      
       <div className={styles.title}>
         <h1>
           hello
