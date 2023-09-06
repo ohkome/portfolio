7036fc240592f1fa8173f3cab91288b2b2318592
ohkome <workskome@gmail.com>
2023-09-07 00:41:00 +0900

    メインビジュアルのレイアウトを修正

--- src/app/compornents/Top/Top.js
+++ src/app/compornents/Top/Top.js
@@ -6,8 +6,7 @@ import styles from './Top.module.css'
 
 
 // コンポーネント
-import Logo from '../Logo/Logo'
-
+import Title from '../Title/Title'
 
 const navigationMenu = [
   {
@@ -21,17 +20,40 @@ const navigationMenu = [
   {
     href: "#",
     label: "Works",
+  }
+]
+
+const mainvisualLink = [
+  {
+    href: "#",
+    src: "/github-icon.png",
+    alt: "Github",
   },
+  {
+    href: "#",
+    src: "/resume-icon.png",
+    alt: "PDF resume",
+  }
 ]
 
 const Top = () => {
   return (
-    <div className={styles.grid}>
+    <>
       <header className={styles.header}>
         {/* svg-logo */}
-        <Link href="/" className={styles.link} >
-          <Logo />
-        </Link>
+        <h1>
+          <Link href="/" className={styles.link} >
+            <Image 
+            src='/logo.svg' 
+            alt="me" 
+            priority
+            width={100}
+            height={50}
+            className={styles.logo}
+            />
+          </Link>
+        </h1>
+        
 
         {/* navigationMenu */}
         <nav className={styles.nav}>
@@ -39,33 +61,50 @@ const Top = () => {
             {navigationMenu.map((item)=>{
               return(
                 <li key={item.label} className={styles.item}>
-                <Link href={item.href} className={styles.link}>{item.label}</Link>
-              </li>
+                  <Link href={item.href} className={styles.link}>{item.label}</Link>
+                </li>
               );
             })}
           </ul>
         </nav>
       </header>
-
-      <div className={styles.img}>
-        <Image 
-        src='/photo.png' 
-        alt="me" 
-        priority
-        width={300}
-        height={400}
-        />
-      </div>
       
-      <div className={styles.title}>
-        <h1>
-          hello
-        </h1>
-        <p>
-            This is My Portfolio.
-        </p>
+      {/* mainvisual */}
+      <div className={styles.layout}>
+        <div className={styles.img}>
+          <Image 
+          src='/photo.png' 
+          alt="me" 
+          priority
+          width={200}
+          height={300}
+          />
+        </div>
+
+        <div className={styles.title}>
+          <Title />
+          <p className={styles.text}>
+            Hi! I am Minori Komeichi.<br />
+              Welcome to My Portfolio Website.
+          </p>
+          <div className={styles.link}>
+            {mainvisualLink.map((item) =>{
+              return(
+                <Link key={item.alt} href={item.href}>
+                  <Image 
+                  src={item.src} 
+                  alt={item.alt} 
+                  className={styles.icon} 
+                  width={30} 
+                  height={30} 
+                  />
+                </Link>
+              );
+            })}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
