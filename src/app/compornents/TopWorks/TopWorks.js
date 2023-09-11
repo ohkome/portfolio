3bbbf84160157ef0d66c30d5f0f9fe73dcf65070
ohkome <workskome@gmail.com>
2023-09-11 23:13:45 +0900

    TopWorksのWorks部分を作成

--- src/app/compornents/TopWorks/TopWorks.js
+++ src/app/compornents/TopWorks/TopWorks.js
@@ -1,11 +1,69 @@
 import React from 'react';
-import Link from 'next/link';
+import Link from 'next/link'
+import Image from 'next/image';
 import styles from './TopWorks.module.css';
 // コンポーネント
 import SectionTitle from '../SectionTitle/SectionTitle';
 import BackgroundSlider from '../BackgroundSlider/BackgroundSlider';
 
 const TopWorks = () => {
+
+  const worksDesign = [
+    {
+      src: '/mosya.jpg',
+      alt: '名刺',
+      href: '#'
+    },
+    {
+      src: '/mosya.jpg',
+      alt: 'オンライン英会話のバナー',
+      href: ''
+    },
+    {
+      src: '/mosya.jpg',
+      alt: '店内ポップ',
+      href: ''
+    },
+    {
+      src: '/mosya.jpg',
+      alt: 'ショップカード',
+      href: ''
+    },
+    {
+      src: '/mosya.jpg',
+      alt: '母の日ポストカード',
+      href: ''
+    },
+  ]
+
+  const worksWeb = [
+    {
+      src: '/mosya.jpg',
+      alt: 'LP-高齢者住宅紹介(架空)',
+      href: ''
+    },
+    {
+      src: '/mosya.jpg',
+      alt: 'Webサイト-カフェ(架空)-',
+      href: ''
+    },
+    {
+      src: '/mosya.jpg',
+      alt: 'LP-登山ツアー(架空)',
+      href: ''
+    },
+    {
+      src: '/mosya.jpg',
+      alt: 'Webサイト-ポートフォリオサイト-',
+      href: ''
+    },
+    {
+      src: '/mosya.jpg',
+      alt: 'LP-職業訓練校の一例',
+      href: ''
+    },
+  ]
+
   return (
     <section className={styles.topWorks}>
       <div className={styles.mainvisual}>
@@ -19,24 +77,51 @@ const TopWorks = () => {
           boxColor="#ccc"
         />
         </div>
-        
       </div>
+
       <div className={styles.wrapper}>
+          <div className={styles.layout_flex}>
+            <h3 className={styles.works_tag}>Design</h3>
 
-          <div className={styles.layout}>
-            <ul className={styles.works_tag}>
-              <li>
-                <button>All</button>
-                <button>Design</button>
-                <button>Web</button>
-              </li>
+            <ul className={styles.layout_grid}>
+              {worksDesign.map((item) => {
+                return(
+                  <li className={styles.works_item}>
+                    <Link href={item.href}>
+                      <Image 
+                      src={item.src} 
+                      alt={item.alt} 
+                      fill
+                      sizes='(max-width: 768px) 50%, 100%'
+                      className={styles.works_img}
+                      />
+                    </Link>
+                  </li>
+                )
+              })}
             </ul>
+          </div>
 
-            <div className={styles.text}>テキスト</div>
+          <div className={`${styles.layout_flex} ${styles.reverce}`}>
+            <h3 className={styles.works_tag}>Web</h3>
 
-            <div className={styles.view}>画像</div>
-
-            <div className={styles.menue}>スライダー</div>
+            <ul className={styles.layout_grid}>
+              {worksWeb.map((item) => {
+                return(
+                  <li className={styles.works_item}>
+                    <Link href={item.href}>
+                      <Image 
+                      src={item.src} 
+                      alt={item.alt} 
+                      fill
+                      sizes='(max-width: 768px) 50%, 100%'
+                      className={styles.works_img}
+                      />
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
       </div>
     </section>
